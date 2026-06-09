199fff282fbd896cb379cd762c254ba727716214
root <root@ip-10-42-165-59.ec2.internal>
2026-06-09 08:45:00 +0000

    Add StudentClass

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -40,3 +40,37 @@ const printTeacher: printTeacherFunction = (firstName: string, lastName: string)
 };
 
 console.log(printTeacher('John', 'Doe'));
+
+interface StudentClassInterface {
+  firstName: string;
+  lastName: string;
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  firstName: string;
+
+  lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+const studentClass: StudentClassConstructor = StudentClass;
+
+console.log(studentClass);
