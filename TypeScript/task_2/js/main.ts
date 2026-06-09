77afa355ce192b1b6c3af15722ec3caaf33d6db3
root <root@ip-10-42-165-59.ec2.internal>
2026-06-09 08:51:58 +0000

    Add TypeScript task 2

---
+++ TypeScript/task_2/js/main.ts
@@ -0,0 +1,30 @@
+workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  if (typeof salary === 'number' && salary < 500) {
+    return new Teacher();
+  }
+
+  return new Director();
+}
+
+console.log(createEmployee(200));
+console.log(createEmployee(1000));
+console.log(createEmployee('$500'));
